8b5933ecff48038c0e5cf4dcf513de2d73ef8aa6
raphael f alves <raphaelfranciscodev@gmail.com>
2026-05-09 10:07:38 -0300

    feat: read csv

--- VehicleMaintenance/Program.cs
+++ VehicleMaintenance/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CsvHelper;
+using System.Globalization;
+
+using var reader = new StreamReader("vehicle.csv");
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+var records = csv.GetRecords<Vehicle>();
+
+foreach (var record in records.ToList())
+{
+    Console.WriteLine(record.Model);
+}
+
+class Vehicle
+{
+    public string Model { get; set; } = null!;
+    public string Brand { get; set; } = null!;
+    public int Year { get; set; }
+    public int CurrentMileage { get; set; }
+    public int LastOilChangeMileage { get; set; }
+    public int LastTireChangeMileage { get; set; }
+    public int LastRevisionMileage { get; set; }
+}
