3b60fae3fcc2af71852abda2be5d6952994f5a18
raphael f alves <raphaelfranciscodev@gmail.com>
2026-05-10 07:34:53 -0300

    feat: add maintenance rules

--- VehicleMaintenance/Program.cs
+++ VehicleMaintenance/Program.cs
@@ -5,10 +5,23 @@ using var reader = new StreamReader("vehicle.csv");
 using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 var records = csv.GetRecords<Vehicle>();
 
-foreach (var record in records.ToList())
-{
-    Console.WriteLine(record.Model);
-}
+var vehicle = records.FirstOrDefault();
+
+if (vehicle is null)
+    return;
+
+string context = string.Empty;
+
+if (vehicle.CurrentMileage - vehicle.LastOilChangeMileage >= 10000)
+    context += "precisa trocar o óleo, pois trocar a cada 10.000km;";
+
+if (vehicle.CurrentMileage - vehicle.LastTireChangeMileage >= 20000)
+    context += "precisa revisar o pneu, pois revisar a cada 20.000km;";
+
+if (vehicle.CurrentMileage - vehicle.LastRevisionMileage >= 30000)
+    context += "precisa fazer revisão geral, pois revisar a cada 30.000km;";
+
+Console.WriteLine(context);
 
 class Vehicle
 {
